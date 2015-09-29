d64de36bd9dc319bd6b9ff0fa995f646bbb3e494
dowast92 <dowast92@gmail.com>
2015-09-29 13:41:34 +0000

    postcss-autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
-var gulp = require("gulp"),
-    jade = require('gulp-jade-php'),
-    sass = require('gulp-sass'),
-	imagemin = require('gulp-imagemin'),
-    livereload = require('gulp-livereload');
-    var default_path = {
-      src: './',
-      dst: '../1c-lp/',
-    }
-    var path = {
-      src : {
-        scss: default_path.src+'scss/**/*',
-        js: default_path.src+'js/**/*',
+var gulp         = require("gulp"),
+    jade 				 = require('gulp-jade-php'),
+    sass         = require('gulp-sass'),
+		postcss      = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
+		imagemin     = require('gulp-imagemin'),
+		sourcemaps   = require('gulp-sourcemaps'),
+    livereload   = require('gulp-livereload');
+var default_path = {
+  src: './',
+  dst: '../1c-lp/',
+}
+var path = {
+  src : {
+    scss: default_path.src+'scss/**/*',
+    css: default_path.src+'css-src/*.css',
+
+    js: default_path.src+'js/**/*',
 		img: default_path.src+'img/**/*',
 		fonts:default_path.src+'fonts/**/*',
-        components: default_path.src+'components/**/*',
-        template: default_path.src+'components/**/template.jade'
-      },
-      dst : {
-        scss: default_path.dst+'css/',
-        js: default_path.dst+'js/',
-		img:default_path.dst+'img/',
-		fonts: default_path.dst+'fonts/',
-        components: default_path.dst+'components/',
-        template: default_path.src+'components/'
-      }
-    }
-gulp.task('default',['index-php', 'scss', 'js','img','fonts', 'components', 'template', 'watch']);
+    components: default_path.src+'components/**/*',
+    template: default_path.src+'components/**/template.jade'
+  },
+  dst : {
+    scss: default_path.dst+'css/',
+    js: default_path.dst+'js/',
+img:default_path.dst+'img/',
+fonts: default_path.dst+'fonts/',
+    components: default_path.dst+'components/',
+    template: default_path.src+'components/'
+  }
+}
+gulp.task('default',['index-php', 'scss', 'autoprefixer','js','img','fonts', 'components', 'template', 'watch']);
 
 gulp.task('index-php', function() {
   gulp.src('./jade/*.jade')
@@ -44,7 +49,7 @@ gulp.task('scss',function() {
 		// 	}))
 		.pipe(sass().on('error', sass.logError))
 		.pipe(sass({errLogToConsole: true}))
-		.pipe(gulp.dest(path.dst.scss))
+		.pipe(gulp.dest('./css-src/'))
 		.pipe(livereload())
 });
 gulp.task('img',function() {
@@ -77,9 +82,18 @@ gulp.task('template', function () {
     .pipe(livereload())
 });
 
+gulp.task('autoprefixer', function () {
+    return gulp.src(path.src.css)
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions','ie 8', 'ie 9'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.dst.scss))
+				.pipe(livereload());
+});
 gulp.task('watch', function() {
 	livereload.listen()
 	gulp.watch(path.src.scss, ['scss']);
+	gulp.watch(path.src.css, ['autoprefixer']);
 	gulp.watch(path.src.js, ['js']);
 	gulp.watch(path.src.components, ['components']);
 	gulp.watch(path.src.template, ['template']);
